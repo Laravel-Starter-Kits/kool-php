ebe2c547e833bae560d3b62fef2c6f3fb98d941e
dshyam3001 <dasarishyam527@gmail.com>
2023-01-14 14:27:52 +0530

    xdebu set to true and env loaded in vite.conf

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-const host = 'kool-php.test';
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    server: {
-        https: false,
-        host: true,
-        port: 3000,
-        hmr: { host: host, protocol: 'ws' },
-    },
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    const host = env.APP_URL;
+    return {
+        plugins: [
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+            {
+                name: 'blade',
+                handleHotUpdate({ file, server }) {
+                    if (file.endsWith('.blade.php')) {
+                        server.ws.send({
+                            type: 'full-reload',
+                            path: '*',
+                        });
+                    }
+                },
+            },
+        ],
+        server: {
+            https: false,
+            host: true,
+            port: 3000,
+            hmr: { host: host, protocol: 'ws' },
+        },
+    }
 });
